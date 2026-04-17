dca2fa57a4ce568af3924e95ab8c02ee8b4c65d2
Matheus <145494214+cc23149@users.noreply.github.com>
2026-04-17 11:35:12 -0300

    commitPronto

--- main.c
+++ main.c
@@ -1,55 +1,43 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
-// função para identificar palavras reservadas
-void palavraReservada(char *str) {
-    if (strcmp(str, "program") == 0) printf("programa\n");
-    else if (strcmp(str, "label") == 0) printf("rotulo\n");
-    else if (strcmp(str, "type") == 0) printf("tipo\n");
-    else if (strcmp(str, "var") == 0) printf("variavel\n");
-    else if (strcmp(str, "procedure") == 0) printf("procedimento\n");
-    else if (strcmp(str, "function") == 0) printf("funcao\n");
-    else if (strcmp(str, "begin") == 0) printf("inicio\n");
-    else if (strcmp(str, "end") == 0) printf("fim\n");
-    else if (strcmp(str, "if") == 0) printf("se\n");
-    else if (strcmp(str, "then") == 0) printf("entao\n");
-    else if (strcmp(str, "else") == 0) printf("senao\n");
-    else if (strcmp(str, "while") == 0) printf("enquanto\n");
-    else if (strcmp(str, "do") == 0) printf("faca\n");
-    else if (strcmp(str, "goto") == 0) printf("vapara\n");
-    else if (strcmp(str, "and") == 0) printf("e\n");
-    else if (strcmp(str, "or") == 0) printf("ou\n");
-    else if (strcmp(str, "not") == 0) printf("nao\n");
-    else if (strcmp(str, "div") == 0) printf("dividir\n");
-    else printf("identificador\n");
-}
+// ================= ENUM DE TOKENS =================
+typedef enum {
+    PROGRAMA,
+    IDENTIFICADOR,
+    NUMERO,
+    PONTO_E_VIRGULA,
+    ATRIBUICAO,
+    INICIO,
+    FIM,
+    SE,
+    ENTAO,
+    MAIS,
+    MAIOROUIGUAL,
+    VIRGULA,
+    ABRE_PARENTESES,
+    FECHA_PARENTESES,
+    PONTO,
+    FIM_DE_ARQUIVO,
+    DESCONHECIDO
+} Token;
 
-int main() {
-    FILE *fp = fopen("entrada.txt", "r");
+// ================= VARIÁVEL GLOBAL =================
+FILE *fp;
+
+// ================= ANALISADOR LÉXICO =================
+Token analex() {
     char c;
     char buffer[100];
     int i;
 
-    if (fp == NULL) {
-        printf("Erro ao abrir arquivo\n");
-        return 1;
-    }
-
     while ((c = fgetc(fp)) != EOF) {
 
-        // ignorar espaços
         if (isspace(c)) continue;
 
-        // identificadores ou palavras reservadas
+        // IDENTIFICADOR OU PALAVRA RESERVADA
         if (isalpha(c)) {
             i = 0;
             buffer[i++] = c;
@@ -59,69 +47,122 @@ int main() {
             }
 
             buffer[i] = '\0';
-
-            palavraReservada(buffer);
-
             ungetc(c, fp);
+
+            if (strcmp(buffer, "program") == 0) return PROGRAMA;
+            else if (strcmp(buffer, "begin") == 0) return INICIO;
+            else if (strcmp(buffer, "end") == 0) return FIM;
+            else if (strcmp(buffer, "if") == 0) return SE;
+            else if (strcmp(buffer, "then") == 0) return ENTAO;
+            else return IDENTIFICADOR;
         }
 
-        // números
+        // NÚMERO
         else if (isdigit(c)) {
             while (isdigit(c = fgetc(fp)));
-            printf("numero\n");
             ungetc(c, fp);
+            return NUMERO;
         }
 
-        // operadores e símbolos
+        // OPERADORES E SÍMBOLOS
         else {
             switch (c) {
-
-                case '+': printf("mais\n"); break;
-                case '-': printf("menos\n"); break;
-                case '*': printf("vezes\n"); break;
-                case '=': printf("igual\n"); break;
-
-                case '<':
-                    c = fgetc(fp);
-                    if (c == '=') printf("menorouigual\n");
-                    else if (c == '>') printf("diferente\n");
-                    else {
-                        printf("menor\n");
-                        ungetc(c, fp);
-                    }
-                    break;
+                case '+': return MAIS;
 
                 case '>':
                     c = fgetc(fp);
-                    if (c == '=') printf("maiorouigual\n");
-                    else {
-                        printf("maior\n");
-                        ungetc(c, fp);
-                    }
+                    if (c == '=') return MAIOROUIGUAL;
+                    ungetc(c, fp);
                     break;
+
+                case ';': return PONTO_E_VIRGULA;
+                case '.': return PONTO;
+                case ',': return VIRGULA;
+                case '(': return ABRE_PARENTESES;
+                case ')': return FECHA_PARENTESES;
 
                 case ':':
                     c = fgetc(fp);
-                    if (c == '=') printf("atribuicao\n");
-                    else {
-                        printf("doispontos\n");
-                        ungetc(c, fp);
-                    }
+                    if (c == '=') return ATRIBUICAO;
+                    ungetc(c, fp);
                     break;
-
-                case '(': printf("abreparenteses\n"); break;
-                case ')': printf("fechaparenteses\n"); break;
-                case '[': printf("abrecolchetes\n"); break;
-                case ']': printf("fechacolchetes\n"); break;
-                case ',': printf("virgula\n"); break;
-                case ';': printf("pontoevirgula\n"); break;
-                case '.': printf("ponto\n"); break;
-
-                default:
-                    printf("simbolo desconhecido: %c\n", c);
             }
         }
+
+        return DESCONHECIDO;
     }
+
+    return FIM_DE_ARQUIVO;
+}
+
+// ================= BLOCO =================
+void compila_bloco() {
+    Token token;
+
+    token = analex();
+    if (token != INICIO) {
+        printf("Erro: esperava BEGIN\n");
+        exit(1);
+    }
+
+    // Aqui poderia ter comandos (simplificado)
+
+    token = analex();
+    if (token != FIM) {
+        printf("Erro: esperava END\n");
+        exit(1);
+    }
+}
+
+// ================= PROGRAMA =================
+void compila_programa() {
+    Token token;
+
+    token = analex();
+    if (token != PROGRAMA) {
+        printf("Erro: esperava PROGRAM\n");
+        exit(1);
+    }
+
+    token = analex();
+    if (token != IDENTIFICADOR) {
+        printf("Erro: esperava identificador\n");
+        exit(1);
+    }
+
+    token = analex();
+    if (token != PONTO_E_VIRGULA) {
+        printf("Erro: esperava ;\n");
+        exit(1);
+    }
+
+    compila_bloco();
+
+    token = analex();
+    if (token != PONTO) {
+        printf("Erro: esperava .\n");
+        exit(1);
+    }
+
+    token = analex();
+    if (token != FIM_DE_ARQUIVO) {
+        printf("Erro: esperava fim de arquivo\n");
+        exit(1);
+    }
+
+    printf("Programa sintaticamente correto!\n");
+}
+
+// ================= MAIN =================
+int main() {
+    fp = fopen("entrada.txt", "r");
+
+    if (fp == NULL) {
+        printf("Erro ao abrir arquivo\n");
+        return 1;
+    }
+
+    compila_programa();
 
     fclose(fp);
     return 0;
